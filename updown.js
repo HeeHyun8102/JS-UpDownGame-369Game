6af5221d6b092b88769c2625b1877b098511452c
Heehyun8102 <k2h0395@gmail.com>
2020-05-22 20:25:36 +0900

    game

---
+++ updown.js
@@ -0,0 +1,35 @@
+const range=100;
+const answer=Math.ceil(Math.random()*range);
+console.log(answer);
+
+const inputTag = document.getElementById("input");
+const resultDiv = document.getElementById("result");
+const formBtn = document.getElementById("formbtn");
+const displayLife = document.getElementById("lifecnt");
+
+let life = '5';
+formBtn.onclick =(event) => {
+    life -= 1;
+    displayLife.textContent = life;
+    const userInput = Number(inputTag.value);
+    const result = document.createElement('h2');
+    result.textContent = userInput + "=>";
+
+    if (userInput === answer){
+        result.textContent += 'Answer';
+        alert('You are correct!');
+    }
+    else{
+        if(life > 0){
+            event.preventDefault();
+            (userInput > answer) ? (result.textContent += 'Down'):(result.textContent += 'up');
+            inputTag.focus();
+        }
+        else{
+            alert('You failed\nThe answer is ' + answer);
+        }
+    }
+    resultDiv.append(result);
+    inputTag.value = ' ';
+}
+
